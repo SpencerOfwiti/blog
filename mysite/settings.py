44d820eb4b44ceaadbd97551c6e0481508b1cace
SpencerOfwiti <maxspencer56@gmail.com>
2019-11-26 13:12:51 +0300

    Added PostgreSQL to project

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spencers_blog',
+        'USER': 'blog',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
